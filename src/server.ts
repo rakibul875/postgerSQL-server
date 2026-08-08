cffc0ead79caff791b399d0a5b930de828f9d5e9
rakibul875 <rakibulislamashik78@gmail.com>
2026-08-08 20:35:21 +0600

    update package.json scripts and restructure server startup

--- src/server.ts
+++ src/server.ts
@@ -10,5 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // app.use("/", routes);
-
-app.listen(process.env.PORT || 5000);
+const startServer =async () => {
+  app.listen(process.env.PORT || 5000);
+};
+startServer();
