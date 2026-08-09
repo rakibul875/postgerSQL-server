3e098df5ae5d87b8937d793b3251c84ff0023186
rakibul875 <rakibulislamashik78@gmail.com>
2026-08-09 09:40:05 +0600

    clean up Prisma schema comments and restructure server initialization

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,9 @@
-import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
-// import routes from "./routes";
+import app from "./app";
 
 dotenv.config();
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-// app.use("/", routes);
-const startServer =async () => {
-  app.listen(process.env.PORT || 5000);
-};
-startServer();
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
