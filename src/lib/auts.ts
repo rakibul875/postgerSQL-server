db53fbfadf00516a2844d6256727ca015ae84dea
rakibul875 <rakibulislamashik78@gmail.com>
2026-08-10 21:43:17 +0600

    normalize UserRole enum values and default role in auth configuration to lowercase

--- src/lib/auts.ts
+++ src/lib/auts.ts
@@ -29,7 +29,7 @@ export const auth = betterAuth({
     additionalFields: {
       role: {
         type: "string",
-        defaultValue: "Customer",
+        defaultValue: "customer",
       },
     },
   },
