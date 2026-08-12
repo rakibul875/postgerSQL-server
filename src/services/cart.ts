b12d40c8386755f395a46994c4e45348b0c2f6a8
rakibul875 <rakibulislamashik78@gmail.com>
2026-08-13 00:06:29 +0600

    refactor cart routes to retrieve user-specific cart data and add validation for existing items

--- src/services/cart.ts
+++ src/services/cart.ts
@@ -3,8 +3,13 @@ import prisma from "../lib/prisma";
 
 const cartRouter = Router();
 
-cartRouter.get("/cart", async (req: Request, res: Response) => {
-  const cartData = await prisma.cart.findMany();
+cartRouter.get("/cart/:id", async (req: Request, res: Response) => {
+  const userId = String(req.params.id);
+  const cartData = await prisma.cart.findMany({
+    where: {
+      userId: userId,
+    },
+  });
   res.json({
     success: true,
     message: "Cart data retrieved successfully",
@@ -14,6 +19,18 @@ cartRouter.get("/cart", async (req: Request, res: Response) => {
 
 cartRouter.post("/cart", async (req: Request, res: Response) => {
   const cartData = req.body;
+  const foodId = cartData.foodId;
+  const existingCartItem = await prisma.cart.findFirst({
+    where: {
+      foodId: foodId,
+    },
+  });
+  if (existingCartItem) {
+    return res.status(400).json({
+      success: false,
+      message: "Item already in cart",
+    });
+  }
   const data = await prisma.cart.create({ data: cartData });
   res.json({
     success: true,
