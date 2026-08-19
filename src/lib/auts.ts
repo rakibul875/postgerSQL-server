a48462bc5f7a561522360200e702cd969891f131
rakibul875 <rakibulislamashik78@gmail.com>
2026-08-19 18:29:53 +0600

    Refactor trustedOrigins and clean up comments in auth configuration

--- src/lib/auts.ts
+++ src/lib/auts.ts
@@ -14,14 +14,12 @@ const prisma = new PrismaClient({
   adapter,
 });
 
-// trustedOrigins এর টাইপ সেফটি
 const trustedOrigins = [
   "https://restauranthub-lovat.vercel.app",
   process.env.CLIENT_URL || "http://localhost:5173",
 ];
 
 export const auth = betterAuth({
-  // Backend Render URL উল্লেখ করা প্রয়োজন
   baseURL: process.env.BETTER_AUTH_URL,
 
   trustedOrigins,
@@ -35,13 +33,12 @@ export const auth = betterAuth({
   },
 
   advanced: {
-    // Vercel এবং Render ভিন্ন ডোমেইন হওয়ায় crossSubdomainCookies বন্ধ রাখতে হবে
     crossSubdomainCookies: {
       enabled: false,
     },
     defaultCookieAttributes: {
-      sameSite: "none", // Cross-site request-এর জন্য আবশ্যক
-      secure: true, // HTTPS-এ true হতে হবে
+      sameSite: "none",
+      secure: true,
     },
   },
 
